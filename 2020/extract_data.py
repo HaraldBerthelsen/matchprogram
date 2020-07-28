b27cbb0eaf06d7dc67ecd3e13dd47ca06c8682b7
Harald Berthelsen <harald@stts.se>
2020-07-28 12:35:44 +0200

    hb

--- 2020/extract_data.py
+++ 2020/extract_data.py
@@ -11,6 +11,20 @@ rounds = data.find_all("div", "matchesRounds")
 
 #print(len(rounds))
 
+
+def convert_date(date):
+    #"söndag 14 juni" -> "14/6"
+    months = {
+        "juni":6,
+        "juli":7
+    }
+    (_,day,month_name) = date.split(" ")
+    return "%s/%s" % (day, months[month_name])
+                      
+
+
+
+
 match_info_list = []
 
 #for round in rounds[0:1]:
@@ -34,6 +48,7 @@ for round in rounds:
         match_id = match["class"][1][-4:]
         round = match["data-round"]
         date = match["data-start-time"]
+        date = convert_date(date)
         time = match["data-time-calendar"]
         arena = match.find("div", "arena").text.strip()
         score = match.find("div", "match-score").text
@@ -68,7 +83,7 @@ for round in rounds:
 for match_info in match_info_list:
     match_file = "%s.html" % match_info["id"]
     if os.path.exists(match_file):
-        print("READING %s" % match_file)
+        #print("READING %s" % match_file)
         
         with open(match_file) as fh:
             html_doc = fh.read()
@@ -86,13 +101,23 @@ for match_info in match_info_list:
                 match_info["referee"] = referee
 
 
+        #ATTENDANCE
+        match_info["attendance"] = "0"
+        match_info["attendance_away"] = "0"
+                
+
         #EXTRA TIME
         times = data.find_all("div", "time-after")
-        first_half_extra_time = re.search("\+([0-9]+)'", times[0].text.strip()).group(1)
+        m = re.search("\+([0-9]+)'", times[0].text.strip())
+        if m:
+            first_half_extra_time = m.group(1)
+        else:
+            first_half_extra_time = "0"
         match_info["first_half_extra_time"] = int(first_half_extra_time)
         
         second_half_extra_time = re.search("\+([0-9]+)'", times[2].text.strip()).group(1)
         match_info["second_half_extra_time"] = int(second_half_extra_time)
+        match_info["total_time"] = 90+int(first_half_extra_time)+int(second_half_extra_time)
 
 
         #PLAYERS
@@ -131,7 +156,7 @@ for match_info in match_info_list:
 
             p = {
                 "name": name,
-                "number": number,
+                "number": int(number),
                 "starting": True
                 }
 
@@ -140,7 +165,7 @@ for match_info in match_info_list:
                 time = out.text
                 if time.endswith("'"):
                     #print("UT (%s): %s" % (time[0:-1], name))
-                    p["sub-out"] = time
+                    p["sub-out"] = int(time[0:-1])
                 
             yellow_card = player_info1.find("span", "yellow-card")
             if yellow_card:
@@ -159,7 +184,7 @@ for match_info in match_info_list:
             
             squad.append(p)
             
-            print("%s" % (p))
+            #print("%s" % (p))
             i+=2
             
         i = 0
@@ -175,7 +200,7 @@ for match_info in match_info_list:
 
             p = {
                 "name": name,
-                "number": number,
+                "number": int(number),
                 "starting": False
                 }
 
@@ -184,7 +209,7 @@ for match_info in match_info_list:
                 time = sub_in.text
                 if time.endswith("'"):
                     #print("IN (%s): %s" % (time[0:-1], name))
-                    p["sub-in"] = time
+                    p["sub-in"] = int(time[0:-1])
                 
             yellow_card = player_info1.find("span", "yellow-card")
             if yellow_card:
@@ -203,7 +228,7 @@ for match_info in match_info_list:
 
             squad.append(p)
             
-            print("%s" % (p))
+            #print("%s" % (p))
             i+=2
             
         #sys.exit()
@@ -214,9 +239,27 @@ for match_info in match_info_list:
         print("Missing %s" % match_file)
 
 
-        
 
+
+        
+players = {}
 def print_game_stats():
+
+    #Print header
+    to_print = ["Datum", "Motståndare", "Res", "Publik (bortalag)"]
+    print_info(to_print, {}, nonewline=True)
+
+    for match_info in match_info_list:
+        for player in match_info["squad"]:            
+            players[player["number"]] = player["name"]
+    #players_by_number = sorted(match_info_list[0]["squad"], key=lambda k: k['number'])
+    for nr in sorted(players):
+        print("%s %s\t" % (nr, players[nr]), end='')
+    #for player in players_by_number:
+    #    print("%s %s\t" % (player["number"], player["name"]), end='')
+    print("")
+
+    
     for match_info in match_info_list:
 
         if "referee" in match_info:
@@ -231,28 +274,122 @@ def print_game_stats():
         if "second_half_extra_time" in match_info:
             played_time += match_info["second_half_extra_time"]
 
+        #Datum	Motståndare	Res	Publik (bortalag)	1 Johan Wiland	2 Simon Sandberg	3 Dennis Widgren	4 Odilon Kossounou	4 Richard Magyar	5 David Fällman	6 Darijan Bojanic	7 Imad Khalili	8 Jeppe Andersen	9 Sander Svendsen
+													
+        #18/2	Varbergs BOIS	3-0	11 140 (-)	-	S	U	I		S	B	I	S	S1
 
 
-        print("%s\t%s\t%s\t%20s\t%s\t%s\t%20s\t%20s\t%s\t%s\t%s" % (match_info["id"], match_info["round"], match_info["home/away"], match_info["opponent"], match_info["date"], match_info["time"], match_info["arena"], referee, match_info["hif_score"], match_info["other_score"], played_time))
+        to_print = ["date", "opponent", ["hif_score","-","other_score"],["attendance"," (", "attendance_away", ")"]]
+        print_info(to_print, match_info, nonewline=True)
+        
+        players_this_match = {}
+        for player in match_info["squad"]:
+            players_this_match[player["number"]] = player
+                               
+        for nr in sorted(players):
+            p = "-"
+            y = ""
+            r = ""
+            g = ""
+            if nr in players_this_match:
+                player = players_this_match[nr]
+                if player["starting"]:
+                    p = "S"
+                else:
+                    p = "B"
+                if "sub-out" in player:
+                    p = "U"
+                if "sub-in" in player:
+                    p = "I"
+                if "yellow" in player:
+                    y = "G"
+                if "red" in player:
+                    r = "R"
+                if "goals" in player:
+                    g = player["goals"]
+            print("%s%s%s%s\t" % (p, y, r, g), end='')
+        print("")
+
+        
 
 
-players_by_number = {}
+def print_info(to_print,match_info, nonewline=False):
+    print_list = []
+    for t in to_print:
+        if type(t) == type([]):
+            print_list2 = []
+            for t2 in t:
+                if t2 in match_info:
+                    print_list2.append(match_info[t2])
+                else:
+                    print_list2.append(t2)
+            print_list.append("".join(print_list2))
+                    
+        elif t in match_info:
+            print_list.append(match_info[t])
+        else:
+            print_list.append(t)
+    if not nonewline:
+        print("\t".join(print_list))
+    else:
+        print("\t".join(print_list), end='\t')
+        
+
+
+        
+
 def print_player_stats():
      for match_info in match_info_list:
          for player in match_info["squad"]:
-             update_info(player)
+             update_info(player, match_info["total_time"])
+         #print(players[25])
 
      print_player_stats_by_number()
 
 
-def update_info(player):
-    pass
-    
+def update_info(player, match_time=90):
+    nr = player["number"]
+    #if nr == 25:
+    #    print(player)
+    p = players[nr]
+    if type(p) == type(""):
+        #first appearance if this player        
+        p = {
+            "name": player["name"],
+            "nMatches": 0,
+            "nGoals": 0,
+            "nAssists": 0,
+            "nShots": 0,
+            "nYellow": 0,
+            "nRed": 0,
+            "timePlayed": 0
+        }
+        
+    if player["starting"] or "sub-in" in player:
+            p["nMatches"] += 1 
+    if "goals" in player:
+            p["nGoals"] += int(player["goals"])
+    if "yellow" in player:
+            p["nYellow"] += 1
+    if "red" in player:
+            p["nRed"] += 1
+
+    if "sub-in" in player:
+        p["timePlayed"] += match_time-player["sub-in"]
+    if player["starting"] and "sub-out" in player:
+        p["timePlayed"] += match_time-player["sub-out"]
+    if player["starting"]:
+        p["timePlayed"] += match_time
+            
+    players[nr] = p
+
+            
 def print_player_stats_by_number():
-    for nr in sorted(players_by_number.keys()):
-        #nr	spelare	matcher/mål	assist	skott	speltid minuter	gula kort	röda kort
+    #print(players[25])
+    print("nr                              namn	spel/mål assist	skott speltid gula kort röda kort")
+    for nr in sorted(players):
         #1	Johan Wiland	7/0	0	0	568	0	0
-        print(nr)
+        print("%s\t%30s\t%s/%s\t%s\t%s\t%s\t%s\t%s" % (nr, players[nr]["name"], players[nr]["nMatches"], players[nr]["nGoals"], players[nr]["nAssists"], players[nr]["nShots"], players[nr]["timePlayed"], players[nr]["nYellow"], players[nr]["nRed"]))
     
 
 
