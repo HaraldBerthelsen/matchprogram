3dd1a4d2500ada9230fb0e1d115afd86f56bd026
HaraldBerthelsen <haraldberthelsen@gmail.com>
2021-02-11 15:45:06 +0100

    hb

--- w2021/skrivut_spelarinfo.py
+++ w2021/skrivut_spelarinfo.py
@@ -5,39 +5,71 @@ import sys, re
 
 #HB vad är tredje fältet? prev? används inte..
 
-lines = sys.stdin.readlines()
-for line in lines:
 
-    #print("#".join(re.split("\t+", line.strip())))
+def yearbookstyle(lines):
+    for line in lines:
 
-    try:
-        (nr, name, prev, field, nationality, birthyear, motherclub, profession, tohammarby, fromclub, contract, matches, goals) = re.split("\t+", line.strip())
-    except:
-        print(re.sub("\t", "TAB", line))
-        sys.exit()
+        #print("#".join(re.split("\t+", line.strip())))
+
+        try:
+            (nr, name, prev, field, nationality, birthyear, motherclub, profession, tohammarby, fromclub, contract, matches, goals) = re.split("\t+", line.strip())
+        except:
+            print(re.sub("\t", "TAB", line))
+            sys.exit()
+
+        if tohammarby == "-":            
+            toprint = """
+            %s. %s
+            %s
+            %s %s
+            Moderklubb: %s
+            Kom: Egen produkt
+            Kontrakt till %s
+            Matcher: %s. Mål: %s
+            """ % (int(nr), name.strip(), field, nationality, birthyear, motherclub, contract, int(matches), int(goals))
+
+        else:
+
+            toprint = """
+            %s. %s
+            %s
+            %s %s
+            Moderklubb: %s
+            Kom: %s (%s)
+            Kontrakt till %s
+            Matcher: %s. Mål: %s
+            """ % (int(nr), name.strip(), field, nationality, birthyear, motherclub, int(tohammarby), fromclub, contract, int(matches), int(goals))
+
+        print(toprint)
+
+
+
+def magazinestyle(lines):
+    for line in lines:
+
+        #print("#".join(re.split("\t+", line.strip())))
+
+        try:
+            (nr, name, a, field, nationality, birthyear, birthdate, height, motherclub, clubs, tohammarby, fromclub, contract, national, matches, league, cup, goals) = re.split("\t", line.strip())
+        except:
+            print(re.sub("\t", "TAB", line))
+            sys.exit()
+
+        toprint = """
+%s. %s %s
+%s %s. Längd: %s cm.
+• Moderklubb: %s.
+• Klubbar: %s, %s– Hammarby IF.
+• Kontrakt till: %s. • Landskamper: %s
+• Matcher: Ligaspel: %s. Cupspel: %s. Mål: %s.
+            """ % (int(nr), name.strip(), field, nationality, birthdate, height, motherclub, clubs, tohammarby, contract, national, matches, cup, goals)
+
+        if a == "+":
+            print(toprint)
+
         
-    if tohammarby == "-":            
-        toprint = """
-        %s. %s
-        %s
-        %s %s
-        Moderklubb: %s
-        Kom: Egen produkt
-        Kontrakt till %s
-        Matcher: %s. Mål: %s
-        """ % (int(nr), name.strip(), field, nationality, birthyear, motherclub, contract, int(matches), int(goals))
 
-    else:
-    
-        toprint = """
-        %s. %s
-        %s
-        %s %s
-        Moderklubb: %s
-        Kom: %s (%s)
-        Kontrakt till %s
-        Matcher: %s. Mål: %s
-        """ % (int(nr), name.strip(), field, nationality, birthyear, motherclub, int(tohammarby), fromclub, contract, int(matches), int(goals))
 
-    print(toprint)
-    
+lines = sys.stdin.readlines()[1:]
+#yearbookstyle(lines)
+magazinestyle(lines)
