1a9620842fc8a85ccc6370ed70e7a0cc2e18502d
HaraldBerthelsen <haraldberthelsen@gmail.com>
2021-05-02 23:30:58 +0200

    hb

--- scripts/printSeason.py
+++ scripts/printSeason.py
@@ -164,12 +164,126 @@ def getNrCards(colour, player, season, type=None):
                     cards += 1
     return str(cards)
 
+
+def printMatchInfo(season):
+    for match in season:
+        arena = match["Plats"]
+        if arena == "Söderstadion" and match["Kommentar"] != "DIF hemma":
+            home = "Hammarby"
+            away = match["Motståndare"]
+            homeresult = match["HIF"]
+            awayresult = match["Inte HIF"]
+            homehalftime = match["Halvtid HIF"]
+            awayhalftime = match["Halvtid inte HIF"]
+        else:
+            home = match["Motståndare"]
+            away = "Hammarby"
+            homeresult = match["Inte HIF"]
+            awayresult = match["HIF"]
+            homehalftime = match["Halvtid inte HIF"]
+            awayhalftime = match["Halvtid HIF"]
+        referee = match["Domare"]
+        date = match["Datum"]
+        time = match["Tid"]
+        opponent_goals = getOppGoals(match["Händelser motståndare"])
+        hif_goals = getHifGoals(match)
+
+        out = f"""
+{date} {time}, {arena}: 
+{home}-{away} {homeresult}-{awayresult} ({homehalftime}-{awayhalftime})
+Domare: {referee}
+"""
+        print(out)
+        #print(opponent_goals)
+        #print(hif_goals)
+
+        goals = {**hif_goals, **opponent_goals}
+        for time in sorted(goals.keys()):
+            player = goals[time][1]
+            print(f"X-Y ({time}) {player}") 
+
+
+        lineup = getLineup(match)
+        print(lineup)
+            
+        if date == "01/5":
+            sys.exit()
+
+def getOppGoals(opp_events):
+    goals = {}
+    for info in opp_events.split("-"):
+        #print(info)
+        m = re.match("(O?G)\(([0-9]+),([^,\)]+)(,?[^,\)]*)\)", info)
+        if m:
+            goaltype = m.group(1)
+            time = int(m.group(2))
+            lastname = m.group(3)
+            comment = m.group(4)
+            goals[time] = (goaltype, lastname, comment)
+    return goals
+            
+def getHifGoals(match):
+    goals = {}
+    for player in list(match.keys())[firstplayer:]:
+        #print(player)
+        playerinfo = match[player].split("-")
+        #print(playerinfo)
+        for info in playerinfo:
+            #print(info)
+            m = re.match("(O?G)\(([0-9]+)(,?[^,\)]*)\)", info)
+            if m:
+                goaltype = m.group(1)
+                time = int(m.group(2))
+                lastname = player.split(" ")[-1]
+                comment = m.group(3)
+                goals[time] = (goaltype, lastname, comment)
+    return goals
+
+def getLineup(match):
+    fields = {"B":{}, "M":{}, "A":{}}
+
+    for player in list(match.keys())[firstplayer:]:
+        #print(player)
+        playerinfo = match[player].split("-")
+        #print(playerinfo)
+        for info in playerinfo:
+            #print(info)
+            if info == "K":
+                fields["K"] = player
+            else:
+                m = re.match("([BMA])([0-9])", info)
+                if m:
+                    field = m.group(1)
+                    nr = int(m.group(2))
+                    lastname = player
+                    fields[field][nr] = lastname
+                    if "U" in match[player]:
+                        #print(match[player])
+                        m = re.search("U\(([0-9][0-9]a?)\)", match[player])
+                        subout = m.group(1)
+                        #print(subout)
+                        for player2 in list(match.keys())[firstplayer:]:
+                            if f"I({subout})" in match[player2]:
+                                fields[field][nr] += f" ({player2})"
+
+                                           
+
+    lineup = []
+    lineup.append(fields["K"])
+    lineup.append("-")
+    for field in ["B","M","A"]:
+        for pos in sorted(fields[field].keys()):
+            lineup.append(fields[field][pos])
+        lineup.append("-")
+    return lineup
+
+
         
 if __name__ == "__main__":
     csvfile = sys.argv[1]
     season = readCsvFile(csvfile)
-    printMatchStatistics(season)
+    #printMatchStatistics(season)
     #printPlayerStatistics(season, type="Allsvenskan")
-    printPlayerStatistics(season)
-    #printMatchInfo(season)
+    #printPlayerStatistics(season)
+    printMatchInfo(season)
     
